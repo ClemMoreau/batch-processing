94eeee93bc984ac119448a533660690f8d83465e
MOREAU-CL <clement.moreau@prestataire.sihm.fr>
2023-12-12 15:37:56 +0100

    Lesson 2 : Implement Your First Job

--- src/test/java/example/billingjob/BillingJobApplicationTests.java
+++ src/test/java/example/billingjob/BillingJobApplicationTests.java
@@ -1,13 +1,33 @@
 package example.billingjob;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.batch.core.*;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.system.CapturedOutput;
+import org.springframework.boot.test.system.OutputCaptureExtension;
 
 @SpringBootTest
+@ExtendWith(OutputCaptureExtension.class)
 class BillingJobApplicationTests {
 
+	@Autowired
+	private Job job;
+
+	@Autowired
+	private JobLauncher jobLauncher;
+
 	@Test
-	void contextLoads() {
+	void testJobExecution(CapturedOutput output) throws Exception {
+
+		JobParameters jobParameters = new JobParameters();
+		JobExecution jobExecution = jobLauncher.run(job, jobParameters);
+
+		Assertions.assertTrue(output.getOut().contains("processing billing information"));
+		Assertions.assertEquals(ExitStatus.COMPLETED, jobExecution.getExitStatus());
 	}
 
 }
