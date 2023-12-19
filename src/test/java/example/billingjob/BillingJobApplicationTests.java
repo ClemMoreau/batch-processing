98b07ac62ddf32143c24a3a0b7a6ca4aa24ab898
MOREAU-CL <clement.moreau@prestataire.sihm.fr>
2023-12-19 12:04:57 +0100

    Lesson 3 : Testing your Job

--- src/test/java/example/billingjob/BillingJobApplicationTests.java
+++ src/test/java/example/billingjob/BillingJobApplicationTests.java
@@ -1,33 +1,42 @@
 package example.billingjob;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.batch.core.*;
 import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.test.JobLauncherTestUtils;
+import org.springframework.batch.test.JobRepositoryTestUtils;
+import org.springframework.batch.test.context.SpringBatchTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.system.CapturedOutput;
 import org.springframework.boot.test.system.OutputCaptureExtension;
 
-
+@SpringBatchTest
 @SpringBootTest
 @ExtendWith(OutputCaptureExtension.class)
 class BillingJobApplicationTests {
 
 	@Autowired
-	private Job job;
+	private JobLauncherTestUtils jobLauncherTestUtils;
 
 	@Autowired
-	private JobLauncher jobLauncher;
+	private JobRepositoryTestUtils jobRepositoryTestUtils;
+
+	@BeforeEach
+	public void setUp() {
+		jobRepositoryTestUtils.removeJobExecutions();
+	}
 
 	@Test
 	void testJobExecution(CapturedOutput output) throws Exception {
 
-		JobParameters jobParameters = new JobParametersBuilder()
+		JobParameters jobParameters = jobLauncherTestUtils.getUniqueJobParametersBuilder()
 				.addString("input.file", "/some/input/file")
 				.toJobParameters();
-		JobExecution jobExecution = jobLauncher.run(job, jobParameters);
+		JobExecution jobExecution = jobLauncherTestUtils.launchJob(jobParameters);
 
 		Assertions.assertTrue(output.getOut().contains("processing billing information from file /some/input/file"));
 		Assertions.assertEquals(ExitStatus.COMPLETED, jobExecution.getExitStatus());
